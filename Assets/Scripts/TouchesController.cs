efb9971581a4b37e78d4c038604941c0c93e11d0
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-10-28 19:34:37 +0900

    touch event

---
+++ Assets/Scripts/TouchesController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchesController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        DragRecognizer dragRecognizer = GetComponent<DragRecognizer>();
+        dragRecognizer.OnGesture += onDrag;
+	}
+	
+    #region FingerGestures Drag-Action
+    void onDrag ( DragGesture dragGesture )
+    {
+        Debug.Log (" drag ");
+    }
+    #endregion
+}
