5a4860d3ae42cc31f64be5f470a466f672121e04
yunjie.zhang <yunjie.zhang@drecom.co.jp>
2014-10-29 01:32:49 +0900

    texture

--- Assets/Scripts/TouchesController.cs
+++ Assets/Scripts/TouchesController.cs
@@ -1,19 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TouchesController : MonoBehaviour {
+public class TouchesController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         DragRecognizer dragRecognizer = GetComponent<DragRecognizer>();
         dragRecognizer.OnGesture += onDrag;
-	}
-	
+
+        FingerDownDetector fingerDownDector = GetComponent<FingerDownDetector>();
+        fingerDownDector += onFingerDown;
+    }
+    
     #region FingerGestures Drag-Action
-    void onDrag ( DragGesture dragGesture )
+    void onDrag(DragGesture dragGesture)
     {
-        Debug.Log (" drag ");
+        // First Finger
+        FingerGestures.Finger finger = dragGesture.Fingers [0];
+        if (dragGesture == ContinuousGesturePhase.Started)
+        {
+            if (dragGesture.Selection == GameObject.FindWithTag("Player"))
+            {
+            }
+            else
+            {
+            }
+        }
+    }
+    #endregion
+
+    #region FingerDownEvent FingerDown-Action
+    void onFingerDown(FingerDownEvent fingerDownEvent)
+    {
+        Debug.Log(" Finger Down");
     }
     #endregion
 }
