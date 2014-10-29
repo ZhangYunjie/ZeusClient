db0129b4e991fcbe153895ff40085e66c7dc7ef5
yan_runchen <runchen.yan@drecom.co.jp>
2014-10-29 14:54:45 +0900

    add turn skill

---
+++ Assets/Scripts/Skills/Rightturn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rightturn : Skill {
+    public float turnPower = 200;
+    public override void launch(){ 
+        Debug.Log(haveLaunched);
+        if (haveLaunched)
+            return;
+        
+        Debug.Log("Rightturn");
+        haveLaunched = true;
+        
+        Vector2 speedDirection = new Vector2(player.rigidbody.velocity.x, player.rigidbody.velocity.z);
+        
+        player.rigidbody.AddTorque(player.rigidbody.velocity.normalized * turnPower * -1);
+    }
+}
